afd323d0c73dadde27288979a416d1074abf0c25
Bruce MacDonald <brucewmacdonald@gmail.com>
2025-04-01 14:09:41 -0700

    text model forward pass

--- model/models/models.go
+++ model/models/models.go
@@ -5,4 +5,5 @@ import (
 	_ "github.com/ollama/ollama/model/models/gemma3"
 	_ "github.com/ollama/ollama/model/models/llama"
 	_ "github.com/ollama/ollama/model/models/mllama"
+	_ "github.com/ollama/ollama/model/models/qwen25vl"
 )
